4c1f81dd28b13f621a684333806f907d1d1c91ba
Amey Band <35392880+Amey23@users.noreply.github.com>
2020-06-17 23:13:06 +0530

    Create get_tweets.py

---
+++ Extract Twitter Data/get_tweets.py
@@ -0,0 +1,38 @@
+#import pandas as pd
+#import numpy as np
+import tweepy
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler("2xxVGD18npYidiG1IDpz1PSGF", "aYocWtYkBkhH03P0fjfSo2zLpwLC8B4Idm9VT1aJfDqc6YRvRU")
+auth.set_access_token("1005727485831479296-FEUSmzBYlUcoyN9iJJoBnkfNL9OotI","45wNdlmpwtF9rsG9dtPNefs1O0xS9nSWZ7N04ialeBSVh")
+api = tweepy.API(auth, wait_on_rate_limit=True)
+# test authentication
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    print("Error during authentication")
+    
+dt = pd.DataFrame(columns = ['Tweets', 'User','User_location','fav_count', 'rt_count', 'tweet_date'])
+
+i = 0
+for tweet in tweepy.Cursor(api.search, q="#COVID19India", count=100, lang='en').items():
+  print(i, end='\r')
+  dt.loc[i, 'Tweets'] = tweet.text
+  dt.loc[i, 'User'] = tweet.user.name  
+  #dt.loc[i, 'User_statuses_count'] = tweet.user.statuses_count
+  #df.loc[i, 'user_followers'] = tweet.user.followers_count
+  dt.loc[i, 'User_location'] = tweet.user.location
+  #dt.loc[i, 'User_verified'] = tweet.user.verified
+  dt.loc[i, 'fav_count'] = tweet.favorite_count
+  dt.loc[i, 'rt_count'] = tweet.retweet_count
+  dt.loc[i, 'tweet_date'] = tweet.created_at
+  i+=1
+  if i == 100:
+    break
+  else:
+    pass
+
+
+dt.head()
+dt.to_csv("tweet_#Covid19Indianew_.csv",index=False)
